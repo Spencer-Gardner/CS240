d861b21b54ec7643a5dc99e24e8e4278a55b3b5c
Spencer-Gardner <smg223@byu.edu>
2024-01-30 11:34:42 -0700

    Converted ChessMove.java to record

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -8,18 +8,7 @@ import java.util.Objects;
  * Note: You can add to this class, but you may not alter
  * signature of the existing methods.
  */
-public class ChessMove {
-
-    private final ChessPosition startPosition;
-    private final ChessPosition endPosition;
-    private final ChessPiece.PieceType promotionPiece;
-
-    public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
-                     ChessPiece.PieceType promotionPiece) {
-        this.startPosition = startPosition;
-        this.endPosition = endPosition;
-        this.promotionPiece = promotionPiece;
-    }
+public record ChessMove(ChessPosition startPosition, ChessPosition endPosition, ChessPiece.PieceType promotionPiece) {
 
     @Override
     public boolean equals(Object o) {
@@ -27,11 +16,6 @@ public class ChessMove {
         if (o == null || getClass() != o.getClass()) return false;
         ChessMove chessMove = (ChessMove) o;
         return Objects.equals(startPosition, chessMove.startPosition) && Objects.equals(endPosition, chessMove.endPosition) && promotionPiece == chessMove.promotionPiece;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(startPosition, endPosition, promotionPiece);
     }
 
     @Override
@@ -46,14 +30,16 @@ public class ChessMove {
     /**
      * @return ChessPosition of starting location
      */
-    public ChessPosition getStartPosition() {
+    @Override
+    public ChessPosition startPosition() {
         return startPosition;
     }
 
     /**
      * @return ChessPosition of ending location
      */
-    public ChessPosition getEndPosition() {
+    @Override
+    public ChessPosition endPosition() {
         return endPosition;
     }
 
@@ -63,7 +49,8 @@ public class ChessMove {
      *
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
-    public ChessPiece.PieceType getPromotionPiece() {
+    @Override
+    public ChessPiece.PieceType promotionPiece() {
         return promotionPiece;
     }
 }
