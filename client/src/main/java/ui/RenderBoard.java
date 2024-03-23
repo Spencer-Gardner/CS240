32b7ce35f2d83cead21e47a8064afb151b8d0043
Spencer-Gardner <smg223@byu.edu>
2024-03-23 13:09:20 -0600

    Updated chess board with number and letter references

--- client/src/main/java/ui/RenderBoard.java
+++ client/src/main/java/ui/RenderBoard.java
@@ -9,28 +9,34 @@ import chess.ChessPosition;
 
 
 public class RenderBoard {
-    private static final int BOARD_SIZE = 8;
+    private static final String[] LETTERS = {"a", "b", "c", "d", "e", "f", "g", "h"};
 
     private static final Map<String, String> PIECE_MAP = new HashMap<>();
     static {
         PIECE_MAP.put("WHITE_KING", SET_TEXT_BOLD + " K ");
         PIECE_MAP.put("WHITE_QUEEN", SET_TEXT_BOLD + " Q ");
         PIECE_MAP.put("WHITE_BISHOP", SET_TEXT_BOLD + " B ");
-        PIECE_MAP.put("WHITE_KNIGHT", SET_TEXT_BOLD + " K ");
+        PIECE_MAP.put("WHITE_KNIGHT", SET_TEXT_BOLD + " N ");
         PIECE_MAP.put("WHITE_ROOK", SET_TEXT_BOLD + " R ");
         PIECE_MAP.put("WHITE_PAWN", SET_TEXT_BOLD + " P ");
         PIECE_MAP.put("BLACK_KING", SET_TEXT_BOLD + SET_TEXT_COLOR_BLACK + " K ");
         PIECE_MAP.put("BLACK_QUEEN", SET_TEXT_BOLD + SET_TEXT_COLOR_BLACK + " Q ");
         PIECE_MAP.put("BLACK_BISHOP", SET_TEXT_BOLD + SET_TEXT_COLOR_BLACK + " B ");
-        PIECE_MAP.put("BLACK_KNIGHT", SET_TEXT_BOLD + SET_TEXT_COLOR_BLACK + " K ");
+        PIECE_MAP.put("BLACK_KNIGHT", SET_TEXT_BOLD + SET_TEXT_COLOR_BLACK + " N ");
         PIECE_MAP.put("BLACK_ROOK", SET_TEXT_BOLD + SET_TEXT_COLOR_BLACK + " R ");
         PIECE_MAP.put("BLACK_PAWN", SET_TEXT_BOLD + SET_TEXT_COLOR_BLACK + " P ");
     }
 
     public static void drawChessBoard(ChessGame game) {
-        System.out.println(" ");
-        for (int row = 0; row < BOARD_SIZE; row++) {
-            for (int col = 0; col < BOARD_SIZE; col++) {
+        System.out.println("\u001B[0m");
+        System.out.print("  ");
+        for (int i = LETTERS.length - 1; i >= 0; i--) {
+            System.out.print(" " + LETTERS[i] + " ");
+        }
+        System.out.println("\u001B[0m");
+        for (int row = 0; row < 8; row++) {
+            System.out.print((row + 1) + " ");
+            for (int col = 0; col < 8; col++) {
                 if ((row + col) % 2 == 0) {
                     System.out.print(SET_BG_COLOR_DARK_GREEN);
                 } else {
@@ -45,9 +51,15 @@ public class RenderBoard {
             }
             System.out.println("\u001B[0m");
         }
-        System.out.println(" ");
-        for (int row = BOARD_SIZE - 1; row >= 0; row--) {
-            for (int col = BOARD_SIZE - 1; col >= 0; col--) {
+        System.out.println("\u001B[0m");
+        System.out.print("  ");
+        for (String letter : LETTERS) {
+            System.out.print(" " + letter + " ");
+        }
+        System.out.println("\u001B[0m");
+        for (int row = 7; row >= 0; row--) {
+            System.out.print((row + 1) + " ");
+            for (int col = 7; col >= 0; col--) {
                 if ((row + col) % 2 == 0) {
                     System.out.print(SET_BG_COLOR_DARK_GREEN);
                 } else {
@@ -62,7 +74,7 @@ public class RenderBoard {
             }
             System.out.println("\u001B[0m");
         }
-        System.out.println(" ");
+        System.out.println("\u001B[0m");
     }
 
 }
